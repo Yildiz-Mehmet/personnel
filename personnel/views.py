5f4dceb8aaec37f59a6e6f6f00eb83eee029fea2
yıldız <mesel4461@gmail.com>
2023-05-30 13:23:31 +0300

    as

--- personnel/views.py
+++ personnel/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .serializers import DepartmentSerializer,PersonnelSerializer
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from .models import Department,Personnel
 
-# Create your views here.
+
+class DepartmentListView(ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+
+class DepartmentCreateUpdateView(RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class PersonnelListView(ListCreateAPIView):
+    queryset = Personnel.objects.all()
+    serializer_class = PersonnelSerializer
+
+
+
+class PersonnelCreateUpdateView(RetrieveUpdateDestroyAPIView):
+    queryset = Personnel.objects.all()
+    serializer_class = PersonnelSerializer
